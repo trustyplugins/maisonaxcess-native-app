ceca14f496feb334930059f1cb142b034747d650
Joginder1706 <joginder@trustycoders.com>
2024-05-17 15:46:08 +0530

    footer is added

--- src/components/navigation/AppNavigator.js
+++ src/components/navigation/AppNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Platform } from "react-native";
 import Home from '../screens/Home';
 import About from '../screens/About';
 import Signup from '../screens/Signup';
@@ -11,6 +10,7 @@ import Profile from '../screens/Profile';
 import Service from '../screens/Service';
 import { PaperProvider } from 'react-native-paper';
 import OrderSuccess from '../screens/OrderSuccess';
+import Layout from '../common/Layout';
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
@@ -21,78 +21,70 @@ const Navigation = () => {
           header: (props) => <Header {...props} />,
         }}
       >
-        <Stack.Screen
-          name="home"
-          component={Home}
-        />
-        <Stack.Screen
-          name="about"
-          component={About}
-        />
-        <Stack.Screen
-          name="service_types"
-          component={ServiceTypes}
-        />
-        <Stack.Screen
-          name="profile"
-          component={Profile}
-        />
-        <Stack.Screen
-          name="service"
-          component={Service}
-        />
-        <Stack.Screen
-          name="order-success"
-          component={OrderSuccess}
-        />
-        <Stack.Screen
-          name="signup"
-          component={Signup}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="home">
+          {props => (
+            <Layout>
+              <Home {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
 
+        <Stack.Screen name="about">
+          {
+            props => (
+              <Layout>
+                <About {...props} />
+              </Layout>
+            )
+          }
+        </Stack.Screen>
+
+        <Stack.Screen name="service_types">
+          {props => (
+            <Layout>
+              <ServiceTypes {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
+
+        <Stack.Screen name="profile">
+          {props => (
+            <Layout>
+              <Profile {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="service">
+          {props => (
+            <Layout>
+              <Service {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="order-success">
+          {props => (
+            <Layout>
+              <OrderSuccess {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="signup" options={{ headerShown: false }}>
+          {props => (
+            <Layout>
+              <Signup {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="login" options={{ headerShown: false }}>
+          {props => (
+            <Layout>
+              <Login {...props} />
+            </Layout>
+          )}
+        </Stack.Screen>
       </Stack.Navigator>
     </PaperProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  headerButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 10,
-    marginTop: 40,
-    paddingBottom: 10,
-  },
-  headerButtonText: {
-    fontSize: 21,
-    color: 'black',
-    maxWidth: 100,
-    overflow: 'hidden',
-    paddingLeft: 10,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'rgba(0, 0, 0, 0.2)',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.8,
-        shadowRadius: 4,
-      },
-      android: {
-        elevation: 4,
-      },
-    }),
-  },
-  arrowIcon: {
-    marginRight: 5, // Adjust the spacing between the icon and the text
-  },
-});
 
 export default Navigation;
