2b5922c69b45733b0b9cb552e3eafda1e7dd79f6
Joginder1706 <joginder@trustycoders.com>
2024-06-04 13:15:08 +0530

    updated

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,31 +1,11 @@
 // Layout.js
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Keyboard, Platform } from 'react-native';
-import Footer from './Footer';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
 const Layout = ({ children }) => {
-    const [isKeyboardVisible, setKeyboardVisible] = useState(false);
-    useEffect(() => {
-        const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
-            setKeyboardVisible(true);
-        });
-
-        const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
-            setKeyboardVisible(false);
-        });
-
-        // Cleanup listeners on component unmount
-        return () => {
-            keyboardDidHideListener.remove();
-            keyboardDidShowListener.remove();
-        };
-    }, []);
     return (
         <View style={styles.container}>
-            {/* <View style={[styles.content, isKeyboardVisible && styles.contentKeyboardVisible]}> */}
             {children}
-            {/* </View> */}
-            {/* {!isKeyboardVisible && <Footer />} */}
         </View>
     );
 };
@@ -33,15 +13,7 @@ const Layout = ({ children }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // marginBottom: Platform.OS === 'ios' ? 40 : 0,
-    },
-    content: {
-        flex: 1,
-        marginBottom: 10,
-    },
-    contentKeyboardVisible: {
-        marginBottom: 0,
-    },
+    }
 });
 
 export default Layout;
